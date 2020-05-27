4a9aad120d1769e55d93f0745def3244a8260a7e
EngineerACE <temp4you@rambler.ru>
2020-05-27 12:09:40 +0300

    Multiple changes after glukes from GIT

---
+++ jhc/image/GIFOptimizer.java
@@ -0,0 +1,8 @@
+package jhc.image;
+
+public class GIFOptimizer
+{
+    // see https://www.nayuki.io/page/gif-optimizer-java
+    // https://www.nayuki.io/page/qr-code-generator-library
+    // https://www.nayuki.io/page/gaussian-blur-demo
+}
